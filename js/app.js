940bf18ad9aa39343173a82c7fe9738d3722e0d9
unknown <ochoa.m.victor@gmail.com>
2020-04-29 22:05:09 -0700

    Added dynamic ul build

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
+let navbarList = document.querySelector('#navbar__list');
+let sections = document.querySelectorAll('section');
+
+for (let section of sections) {
+  var sectionTitle = document.getElementById(`${section.id}-header`).innerHTML;
+  let newListItem = document.createElement("li");
+  newListItem.innerHTML = sectionTitle;
+  navbarList.append(newListItem);
+}
+
+
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,7 +41,7 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +56,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
